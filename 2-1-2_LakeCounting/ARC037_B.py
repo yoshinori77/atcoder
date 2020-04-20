d76fed5c74fda4c4b100a2d8aa270ac7e92c5e0c
yoshinori77 <yoshinoriyamashita123@gmail.com>
2020-04-20 21:50:02 +0900

    バウムテストをAC

---
+++ 2-1-2_LakeCounting/ARC037_B.py
@@ -0,0 +1,40 @@
+import sys
+
+# sys.setrecursionlimit(10 ** 6)
+
+# input = sys.stdin.readline
+
+
+def dfs(v, p):
+    global closed_path
+
+    seen[v] = True
+    for next_v in g[v]:
+        if next_v == p:
+            continue
+        if seen[next_v]:
+            closed_path = True
+            continue
+        dfs(next_v, v)
+
+
+N, M = map(int, input().split())
+
+g = [[] for _ in range(N+1)]
+seen = {i: False for i in range(N+1)}
+for i in range(M):
+    a, b = map(int, input().split())
+    g[a].append(b)
+    g[b].append(a)
+
+
+cnt = 0
+for v in range(1, N+1):
+    if seen[v]:
+        continue
+    closed_path = False
+    dfs(v, 0)
+    if not closed_path:
+        cnt += 1
+
+print(cnt)
