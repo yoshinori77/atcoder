32f0060575b4aaa8db86decb616cbc4bc3d2c731
yoshinori77 <yoshinoriyamashita123@gmail.com>
2020-05-03 02:37:07 +0900

    Dubious Document 2をAC

---
+++ 2-2-3/ABC076_C.py
@@ -0,0 +1,33 @@
+S = input()
+T = input()
+t = len(T)
+
+for i in range(len(S)-t, -1, -1):
+    print(i)
+    for j in range(t):
+        k = S[i+j]
+        if k != T[j] and k != "?":
+            break
+    else:
+        print((S[:i]+T+S[i+t:]).replace("?", "a"))
+        exit()
+print("UNRESTORABLE")
+
+
+# Sd = input()
+# T = input()
+
+# def check(A, B):
+#     for a, b in zip(A, B):
+#         if a != '?' and a != b:
+#             return False
+
+#     return True
+
+
+# for i in range(len(Sd) - len(T) + 1)[::-1]:
+#     if check(Sd[i:i + len(T)], T):
+#         print((Sd[:i] + T + Sd[i + len(T):]).replace('?', 'a'))
+#         break
+# else:
+#     print('UNRESTORABLE')
