ace229330ea93ef6b05c3ce363aa9e674c0f8955
yoshinori77 <yoshinoriyamashita123@gmail.com>
2020-05-06 18:20:38 +0900

    フィボナッチ数列を動的計画法で解いてみる

---
+++ dp_practise/fibonacci.py
@@ -0,0 +1,60 @@
+import time
+from contextlib import contextmanager
+import argparse
+import sys
+sys.setrecursionlimit(10**9)
+
+
+@contextmanager
+def timer():
+    t0 = time.time()
+    msg = "start"
+    print(msg)
+    yield
+    msg = f"done in {time.time() - t0:.2f} s"
+    print(msg)
+
+
+def fib(n: int):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+
+def fib_memo(n: int):
+    global done
+    global memo
+    if n == 0: return 0
+    if n == 1: return 1
+
+    if done[n]:
+        return memo[n]
+
+    done[n] = True
+    memo[n] = fib_memo(n - 1) + fib_memo(n - 2)
+    return memo[n]
+
+
+def fib_loop(n: int):
+    global memo
+    memo[0] = 0
+    memo[1] = 1
+
+    for i in range(2, n+1):
+        memo[i] = memo[i - 1] + memo[i - 2]
+    return memo[n]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("method", help="Select to solve with which fibonacci method")
+    parser.add_argument("max_num", help="Set max number")
+    args = parser.parse_args()
+
+    N = int(args.max_num)
+    done = [False] * (N + 1)
+    memo = [0] * (N + 1)
+    with timer():
+        print(eval(args.method)(N))
